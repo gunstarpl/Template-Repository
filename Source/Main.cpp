f4983dfb23c6055d07b822ebd5b741d6e0314551
Peter Doan <doanpiotr@gmail.com>
2015-01-24 03:47:05 +0100

    Print "Hello world!" to output.

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -4,6 +4,9 @@ int main(int argc, char* argv[])
 {
     // Initialize debug routines.
     Debug::Initialize();
+    
+    // Output some text.
+    printf("Hello world!\n");
 
     return 0;
 }
--- Source/Precompiled.hpp
+++ Source/Precompiled.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdio.h>
+
 #include "Common/Debug.hpp"
 #include "Common/NonCopyable.hpp"
 #include "Common/ScopeGuard.hpp"
