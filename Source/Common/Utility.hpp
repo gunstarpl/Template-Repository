557932629be88eea6fe1cdbddebbcaefeb128e21
Piotr Doan <doanpiotr@gmail.com>
2015-10-04 01:37:19 +0200

    Remove unused utility functions.

--- Source/Common/Utility.hpp
+++ Source/Common/Utility.hpp
@@ -8,20 +8,6 @@
 
 namespace Utility
 {
-    // Clamps a value between the lower and upper range.
-    template<typename Type>
-    Type Clamp(const Type& value, const Type& lower, const Type& upper)
-    {
-        return std::max(lower, std::min(value, upper));
-    }
-
-    // Frees a container's memory (for STL implementations only).
-    template<typename Type>
-    void ClearContainer(Type& container)
-    {
-        container.swap(Type());
-    }
-
     // Gets the content of a text file.
     std::string GetTextFileContent(std::string filename);
 
